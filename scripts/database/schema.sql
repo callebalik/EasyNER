8263618c1812852d3ecf17ddb9e70a4e8cb7763e
Carl Ollvik Aasa <75130322+callebalik@users.noreply.github.com>
2025-02-12 21:46:09 +0100

    Fix: json->sqlite set entity sentence reference correct on import

--- scripts/database/schema.sql
+++ scripts/database/schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE sentences (
     word_count INTEGER,
     token_count INTEGER,
     alpha_count INTEGER,
-    FOREIGN KEY (document_id) REFERENCES documents (id)
+    FOREIGN KEY (document_id) REFERENCES documents (id),
     PRIMARY KEY (document_id, sentence_index)
 );
 
@@ -22,29 +22,40 @@ CREATE TABLE named_entities (
     named_entity TEXT UNIQUE
 );
 
+CREATE TABLE entity_occurrences_summary (
+    id INTEGER PRIMARY KEY NOT NULL,
+    normalized_entity_text TEXT,
+    fq_uniq_documents INTEGER,
+    fq_sentence_level INTEGER,
+    fq_document_level INTEGER
+);
+
 CREATE TABLE entity_occurrences (
     id INTEGER PRIMARY KEY NOT NULL,
     entity_text TEXT,
     span_start INTEGER,
     span_end INTEGER,
     entity_id INTEGER NOT NULL,
-    sentence_id INTEGER NOT NULL,
+    document_id INTEGER NOT NULL,
+    sentence_index INTEGER NOT NULL,
     summary_id INTEGER,
     intra_doc_fq INTEGER,
     tf REAL,
     inter_doc_fq INTEGER,
     tf_idf REAL,
     idf REAL,
-    FOREIGN KEY (sentence_id) REFERENCES sentences (id),
-    FOREIGN KEY (entity_id) REFERENCES named_entities (id)
-    FOREIGN KEY (summary_id) REFERENCES entity_occurrence_summary (id)
+    FOREIGN KEY (document_id) REFERENCES documents (id),
+    FOREIGN KEY (document_id, sentence_index) REFERENCES sentences (document_id, sentence_index),
+    FOREIGN KEY (entity_id) REFERENCES named_entities (id),
+    FOREIGN KEY (summary_id) REFERENCES entity_occurrences_summary (id)
 );
 
-CREATE TABLE entity_occurrences_summary (
-    id INTEGER PRIMARY KEY NOT NULL,
-    normalized_entity_text TEXT,
-    fq_sentence_level INTEGER,
+CREATE TABLE cooccurrence_summary (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     fq_document_level INTEGER,
+    fq_document_level_normalized REAL,
+    fq_sentence_level INTEGER,
+    fq_sentence_level_normalized REAL
 );
 
 CREATE TABLE entity_cooccurrences (
@@ -60,21 +71,11 @@ CREATE TABLE entity_cooccurrences (
     FOREIGN KEY (coocurences_summary_id) REFERENCES cooccurrence_summary (id)
 );
 
-CREATE TABLE cooccurrence_summary (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    fq_document_level INTEGER,
-    fq_document_level_normalized REAL,
-    fq_sentence_level INTEGER,
-    fq_sentence_level_normalized REAL
-);
-
-CREATE TABLE coentity_summary (
-    id INTEGER PRIMARY KEY NOT NULL
-);
-
 CREATE TABLE source_files (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     file_name TEXT UNIQUE,
     file_size INTEGER,
     import_date DATETIME
 );
+
+
