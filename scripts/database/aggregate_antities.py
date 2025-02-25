37699b1c2a45d39f771fa83d1aef22245beb5ea2
Carl Ollvik Aasa <75130322+callebalik@users.noreply.github.com>
2025-02-25 13:29:09 +0100

    FIX: Update import statement and enhance SLURM to properly use new module strucuture

--- scripts/database/aggregate_antities.py
+++ scripts/database/aggregate_antities.py
@@ -1,8 +1,10 @@
-from db_main import EasyNerDBHandler  # Ensure db_main is imported
+from .db_main import EasyNerDBHandler
 
 db = EasyNerDBHandler()
-# db.analysis.merge_duplicate_aggregated_eos_batched_with_progressbar()   
+# db.analysis.merge_duplicate_aggregated_eos_batched_with_progressbar()
 # db.analysis.optimized_update_entity_summary_ids(queue_size=60, batch_size=50000, num_reader_threads=2)
 # db.analysis.count_entity_cooccurrences_multithreaded()
 # db.analysis.aggregate_entity_cooccurrences_multithreaded()
-db.analysis.co_calc_pmi()
+
+db.co.update_unique_document_counts()
+# db.analysis.co_calc_pmi()
