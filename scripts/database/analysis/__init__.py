29eb963789378b6e68f40fdeb42f0e51fb7302f4
Carl Ollvik Aasa <75130322+callebalik@users.noreply.github.com>
2025-02-28 02:42:06 +0100

    Refactor: Remove unused DBAnalysis class and related imports for cleaner code structure

--- scripts/database/analysis/__init__.py
+++ scripts/database/analysis/__init__.py
@@ -1,7 +1,6 @@
 """
 Database analysis package initialization
 """
-from .db_analysis import DBAnalysis
 from ..data_model.docs import Docs
 from ..data_model.sent import Sentence
 from ..data_model.entities import EntityOccurrence, EntityCooccurence
