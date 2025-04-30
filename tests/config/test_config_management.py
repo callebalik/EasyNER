8268f1b5170588095c73d3d7188b39b729c0559c
Carl Ollvik Aasa <75130322+callebalik@users.noreply.github.com>
2025-04-30 22:37:34 +0200

    fix(config): change store_tokens property type from string to boolean for consistency

--- tests/config/test_config_management.py
+++ tests/config/test_config_management.py
@@ -108,7 +108,7 @@ def sample_config() -> Dict[str, Any]:
             "model_folder": "test_folder",
             "model_name": "test_name",
             "vocab_path": "D:/projects/vocab.txt",
-            "store_tokens": "no",
+            "store_tokens": False,
             "labels": "",
             "clear_old_results": True,
             "article_limit": [-1, 100],
